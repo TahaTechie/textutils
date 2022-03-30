6c76ecbe4ad9ba106d68842aac5c3f2487404e6b
Taha Amir <tahaamir550@gmail.com>
2022-03-30 15:08:21 -0700

    Alerts Add successfully

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
+import Alert from './components/Alert';
+
 import React ,{useState} from 'react';
 
 function App() {
   const [mode, setmode] = useState("light");
+  const [alert, setAlert] = useState(null);
   const changeMode = ()=>{
     if(mode==="light"){
       setmode("dark");
       document.body.style.backgroundColor = "#042743";
       document.body.style.color = "white";
+      showAlert("Dark Mode Enabled","success");
     }
     else{
       setmode("light");
       document.body.style.backgroundColor = "white";
       document.body.style.color = "black";
+      showAlert("Light Mode Enabled","success");
     }
+  }
+
+  const showAlert =(message,type)=>{
+    setAlert({
+      message: message,
+      type:type
+    })
+    setTimeout(()=>{
+      setAlert(null);
+    },1500);
   }
   return (
     <>
       <Navbar title="Text Utils" mode={mode} changeMode={changeMode}/>
-      <Textform heading="Enter Text To Analyze" mode={mode} changeMode={changeMode}/>
+      <Alert alert={alert}/>
+      <Textform heading="Enter Text To Analyze" mode={mode} changeMode={changeMode} showAlert={showAlert}/>
     </>
   );
 }
--- src/components/Textform.jsx
+++ src/components/Textform.jsx
@@ -10,8 +10,9 @@ export default function Textform(props) {
 
     const extractEmail = ()=> {
         setEmails(text.match(/[\w]+@[\w\-]+\.[\w]{2,}/g));
+        props.showAlert("Converted to Upper Case","warning")
     }
-    
+
     return (
         <>
         <div className="container my-3" >
@@ -19,10 +20,10 @@ export default function Textform(props) {
             <div className="mb-3">
                 <textarea className="form-control" onChange={handleChange} value={text} id="myBox" rows="8"></textarea>
             </div>
-            <button className={`btn btn-${props.mode==="light" ? "dark" : "light"} mx-2`} onClick={()=> {setText(text.toUpperCase())}} >Convert to Upper Case</button>
-            <button className={`btn btn-${props.mode==="light" ? "dark" : "light"} mx-2`} onClick={()=> {setText(text.toLowerCase())}} >Convert to Lower Case</button>
+            <button className={`btn btn-${props.mode==="light" ? "dark" : "light"}`} onClick={()=> {setText(text.toUpperCase()); props.showAlert("Converted to Upper Case","info")}} >Convert to Upper Case</button>
+            <button className={`btn btn-${props.mode==="light" ? "dark" : "light"} mx-2`} onClick={()=> {setText(text.toLowerCase()); props.showAlert("Converted to Lower Case","info")}} >Convert to Lower Case</button>
             <button className={`btn btn-${props.mode==="light" ? "dark" : "light"} mx-2`} onClick={extractEmail} >Extract Email</button>
-            <button className={`btn btn-${props.mode==="light" ? "dark" : "light"} mx-2`} onClick={()=> setText("")} >Clear</button>
+            <button className={`btn btn-${props.mode==="light" ? "dark" : "light"} mx-2`} onClick={()=> {setText(""); props.showAlert("Clear Text","danger")}} >Clear</button>
             <h1>Text Summary</h1>
             <p>{text.split(" ").length} Number of Words and {text.length} Number of Characters</p>
             <p>{0.008 * text.split(" ").length} Minutes Required to read</p>
