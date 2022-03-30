fb4b9932b5028aabc94f30e70e509813eb55c8f1
Taha Amir <tahaamir550@gmail.com>
2022-03-30 14:27:03 -0700

    Dark Mode Imporve

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import './App.css';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
+import React ,{useState} from 'react';
 
 function App() {
+  const [mode, setmode] = useState("light");
+  const changeMode = ()=>{
+    if(mode==="light"){
+      setmode("dark");
+      document.body.style.backgroundColor = "#042743";
+      document.body.style.color = "white";
+    }
+    else{
+      setmode("light");
+      document.body.style.backgroundColor = "white";
+      document.body.style.color = "black";
+    }
+  }
   return (
     <>
-      <Navbar title="Text Utils"/>
-      <Textform heading="Enter Text To Analyze" />
+      <Navbar title="Text Utils" mode={mode} changeMode={changeMode}/>
+      <Textform heading="Enter Text To Analyze" mode={mode} changeMode={changeMode}/>
     </>
   );
 }
--- src/components/Textform.jsx
+++ src/components/Textform.jsx
@@ -3,12 +3,6 @@ import React from 'react';
 export default function Textform(props) {
     const [text,setText] = React.useState("Enter Your Text ");
     const [emails,setEmails]= React.useState([]);
-    const [mode, setMode] = React.useState({
-        color:"black",
-        backgroundColor:"white"
-    });
-    const [btnText, setbtnText] = React.useState("Dark Mode");
-    const [changeClass, setchangeClass] = React.useState("btn btn-dark mx-2");
 
     const handleChange = (e)=> {
         setText(e.target.value);
@@ -17,42 +11,18 @@ export default function Textform(props) {
     const extractEmail = ()=> {
         setEmails(text.match(/[\w]+@[\w\-]+\.[\w]{2,}/g));
     }
-
-    const changeMode = ()=> {
-        if(mode.color==="black"){
-            setMode({
-                color:"white",
-                backgroundColor:"black"
-            });
-            setbtnText("Light Mode");
-            setchangeClass("btn btn-light mx-2");
-        }
-        else{
-            setMode({
-                color:"black",
-                backgroundColor:"white"
-            });
-            setbtnText("Dark Mode");
-            setchangeClass("btn btn-dark mx-2"); 
-        }
-    }
-
-
+    
     return (
         <>
-        <div className="form-check form-switch">
-            <input className="form-check-input" onClick={changeMode} type="checkbox" role="switch" />
-            <label className="form-check-label">{btnText}</label>
-        </div>
-        <div className="container my-3" style={mode}>
+        <div className="container my-3" >
             <h1>{props.heading}</h1>
             <div className="mb-3">
                 <textarea className="form-control" onChange={handleChange} value={text} id="myBox" rows="8"></textarea>
             </div>
-            <button className={changeClass} onClick={()=> {setText(text.toUpperCase())}} >Convert to Upper Case</button>
-            <button className={changeClass} onClick={()=> {setText(text.toLowerCase())}} >Convert to Lower Case</button>
-            <button className={changeClass} onClick={extractEmail} >Extract Email</button>
-            <button className={changeClass} onClick={()=> setText("")} >Clear</button>
+            <button className={`btn btn-${props.mode==="light" ? "dark" : "light"} mx-2`} onClick={()=> {setText(text.toUpperCase())}} >Convert to Upper Case</button>
+            <button className={`btn btn-${props.mode==="light" ? "dark" : "light"} mx-2`} onClick={()=> {setText(text.toLowerCase())}} >Convert to Lower Case</button>
+            <button className={`btn btn-${props.mode==="light" ? "dark" : "light"} mx-2`} onClick={extractEmail} >Extract Email</button>
+            <button className={`btn btn-${props.mode==="light" ? "dark" : "light"} mx-2`} onClick={()=> setText("")} >Clear</button>
             <h1>Text Summary</h1>
             <p>{text.split(" ").length} Number of Words and {text.length} Number of Characters</p>
             <p>{0.008 * text.split(" ").length} Minutes Required to read</p>
