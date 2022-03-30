b14ec02232eef6c5606cf9c9a147200a4e4b23cc
Taha Amir <tahaamir550@gmail.com>
2022-03-30 07:22:29 -0700

    useState and Event Handling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Textform from './components/Textform';
 
 function App() {
   return (
     <>
       <Navbar title="Text Utils"/>
+      <Textform heading="Enter Text To Analyze" />
     </>
   );
 }
--- src/components/Textform.jsx
+++ src/components/Textform.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React from 'react';
 
-export default function Textform() {
+export default function Textform(props) {
+    const [text,setText] = React.useState("Enter Your Text ");
+    const handleChange = (e)=> {
+        setText(e.target.value);
+    }
     return (
-        <div className="container">
+        <div className="container my-3">
+            <h1>{props.heading}</h1>
             <div className="mb-3">
-                <label for="myBox" class="form-label">Example textarea</label>
-                <textarea className="form-control" id="myBox" rows="3"></textarea>
+                <textarea className="form-control" onChange={handleChange} value={text} id="myBox" rows="8"></textarea>
             </div>
+            <button className="btn btn-primary"onClick={()=> {setText(text.toUpperCase())}} >Convert to Upper Case</button>
         </div>
     )
 }
