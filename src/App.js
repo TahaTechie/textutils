08f8062fe7164c8a2c2e2c1f07397e5dbf5fc218
Taha Amir <tahaamir550@gmail.com>
2022-03-31 04:02:48 -0700

    react router install and use

--- src/App.js
+++ src/App.js
@@ -1,50 +1,62 @@
+import React from "react";
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
 import Alert from './components/Alert';
+import About from './components/About';
+import { useState } from 'react';
 
-import React ,{useState} from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+
 
 function App() {
   const [mode, setmode] = useState("light");
   const [alert, setAlert] = useState(null);
-  const changeMode = ()=>{
-    if(mode === "light"){
+  const changeMode = () => {
+    if (mode === "light") {
       setmode("dark");
       let c1 = document.getElementById("colorPicker1");
       let c2 = document.getElementById("colorPicker2");
-      document.body.style.backgroundColor = c2.value;
-      document.body.style.color = c1.value;
-      showAlert("Dark Mode Enabled","light");
+      document.body.style.backgroundColor = "dark";
+      document.body.style.color = "white";
+      showAlert("Dark Mode Enabled", "light");
       window.alert("You can also select color for background and foreground");
-      setTimeout(() => {
-        document.title = "TextUtils-Home"
-      }, 2000);
-      setTimeout(() => {
-        document.title = "install TextUtils free"
-      }, 1500);
     }
-    else{
+    else {
       setmode("light");
       document.body.style.backgroundColor = "white";
       document.body.style.color = "black";
-      showAlert("Light Mode Enabled","dark");
+      showAlert("Light Mode Enabled", "dark");
     }
   }
 
-  const showAlert =(message,type)=>{
+  const showAlert = (message, type) => {
     setAlert({
       message: message,
-      type:type
+      type: type
     })
-    setTimeout(()=>{
+    setTimeout(() => {
       setAlert(null);
-    },1500);
+    }, 1500);
   }
   return (
     <>
-      <Navbar title="Text Utils" mode={mode} changeMode={changeMode}/>
-      <Alert alert={alert}/>
-      <Textform heading="Enter Text To Analyze" mode={mode} changeMode={changeMode} showAlert={showAlert}/>
+      <Router>
+        <Navbar title="Text Utils" mode={mode} changeMode={changeMode} />
+        <Alert alert={alert} />
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Textform heading="Enter Text To Analyze" mode={mode} changeMode={changeMode} showAlert={showAlert} />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 }
